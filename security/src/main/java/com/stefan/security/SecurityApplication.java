bd93c8ed3001fc5ae1a98ecdf68c86af2e9dfe4d
Stefan Vasilev <46425449+stefanVasilev17@users.noreply.github.com>
2023-09-22 13:40:05 +0300

    Add files via upload
    upload security project including jwt with RDS aws db -> 3036 port

---
+++ security/src/main/java/com/stefan/security/SecurityApplication.java
@@ -0,0 +1,44 @@
+package com.stefan.security;
+
+import com.stefan.security.auth.AuthenticationService;
+import com.stefan.security.auth.RegisterRequest;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityApplication.class, args);
+
+
+	}
+
+//	@Bean
+//	public CommandLineRunner commandLineRunner(
+//			AuthenticationService service
+//	) {
+//		return args -> {
+//			var admin = RegisterRequest.builder()
+//					.firstname("Admin")
+//					.lastname("Admin")
+//					.email("admin@mail.com")
+//					.password("password")
+//				//	.role(ADMIN)
+//					.build();
+//			System.out.println("Admin token: " + service.register(admin).getToken());
+//
+//			var manager = RegisterRequest.builder()
+//					.firstname("Admin")
+//					.lastname("Admin")
+//					.email("manager@mail.com")
+//					.password("password")
+//				//	.role(MANAGER)
+//					.build();
+//			System.out.println("Manager token: " + service.register(manager).getToken());
+//
+//		};
+//	}
+}
